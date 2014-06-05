4849626d9b4fe55cfa39ee39d80bf9b506c696d0
J.D. Cain <jd.cain.jr@gmail.com>
2014-06-05 16:25:21 -0400

    MAHAPPS Tabs

--- NoteTaker/MainWindow.xaml.cs
+++ NoteTaker/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ namespace NoteTaker
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MahApps.Metro.Controls.MetroWindow
+    public partial class MainWindow
 
     {
         /// <summary>
