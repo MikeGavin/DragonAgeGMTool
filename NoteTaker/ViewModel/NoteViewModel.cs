fb473f0e7e69ac428b604f73ba2bd65242d7d51c
J.D. Cain <jd.cain.jr@gmail.com>
2014-07-31 19:51:01 -0400

    Ordered Note VM

--- NoteTaker/ViewModel/NoteViewModel.cs
+++ NoteTaker/ViewModel/NoteViewModel.cs
@@ -20,54 +20,7 @@ namespace Scrivener.ViewModel
 {
     public class NoteViewModel : ViewModelBase
     {
-        #region Events
-        internal void RaiseTextChanged()
-        {
-            if (TextChanged != null) { TextChanged(this, new EventArgs()); }
-        }
-        public event EventHandler TextChanged;
-
-        /// <summary>
-        /// Raised when this workspace should be removed from the UI.
-        /// </summary>
-        public event EventHandler RequestClose;
-
-        void OnRequestClose()
-        {
-            EventHandler handler = this.RequestClose;
-            if (handler != null)
-                handler(this, EventArgs.Empty);
-        }
-        #endregion
-
-        #region Public Properties
-        private int _saveIndex;
-        public int SaveIndex { get { return _saveIndex; } protected set { _saveIndex = value; RaisePropertyChanged(); } }
-        private string title;
-        public string Title { get { return title; } set { title = value; RaisePropertyChanged(); _titlechanged = true; } }
-        private string text;
-        public string Text { get { return text; } set { text = value; RaisePropertyChanged(); RaiseTextChanged(); } }        
-        #endregion
         private static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
-
-        private static int _number = 0;
-        private bool _titlechanged = false;
-
-        private QuickItem _root;
-        public QuickItem Root { get { return _root; } set { _root = value; RaisePropertyChanged(); } }
-        private QuickItem _selectedQuickItem;
-        public QuickItem SelectedQuickItem { get { return _selectedQuickItem; } set { _selectedQuickItem = value; RaisePropertyChanged(); } }
-        
-
-
-        private RelayCommand _closeNoteCommand;
-        public RelayCommand CloseNoteCommand { get { return _closeNoteCommand ?? (_closeNoteCommand = new RelayCommand(OnRequestClose)); } }
-
-        // local minion instance for this note.
-        private ObservableCollection<MinionCommandItem> _minionCommands;
-        private MinionViewModel _noteMinion;
-        public MinionViewModel NoteMinion { get { return _noteMinion ?? (_noteMinion = new MinionViewModel(_minionCommands)); } set { _noteMinion = value; RaisePropertyChanged(); } }
-
 
         public NoteViewModel(QuickItem _tree, ObservableCollection<MinionCommandItem> commands, int new_index)
         {
@@ -78,18 +31,84 @@ namespace Scrivener.ViewModel
             _root = _tree;
             SaveIndex = new_index;
 
-
             this.TextChanged += Note_TextChanged;
             NoteMinion.MinionCollection.CollectionChanged += MinionCollection_CollectionChanged;
         }
 
+        #region Public Properties
+        private int _saveIndex; // used for ID for note saving
+        public int SaveIndex { get { return _saveIndex; } protected set { _saveIndex = value; RaisePropertyChanged(); } }
+
+        private static int _number = 0; // used to nuber default notes
+        private bool _titlechanged = false; // defines if note title has already been changed
+        private string title;
+        public string Title { get { return title; } set { title = value; RaisePropertyChanged(); _titlechanged = true; } }
+
+        private string text;
+        public string Text { get { return text; } set { text = value; RaisePropertyChanged(); RaiseTextChanged(); } }
+        #endregion        
+
+        #region EventBased Actions
+        //Text change events for note
+        internal void RaiseTextChanged()
+        {
+            if (TextChanged != null) { TextChanged(this, new EventArgs()); }
+        }
+        public event EventHandler TextChanged;
+        //listener runs regex checks on text change
+        void Note_TextChanged(object sender, EventArgs e)
+        {
+            Regex ip = new Regex(@"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b\s+");
+            Regex sepid = new Regex(@"\b[a-z]{2,3}[0-9]{5,6}\b\s+", RegexOptions.IgnoreCase);
+
+
+            MatchCollection mc = ip.Matches(Text);
+
+            foreach (Match m in mc)
+            {
+
+            }
+
+            if (_titlechanged == false) //Changes Title to first SEP entered then stops.
+            {
+                MatchCollection sepmatches = sepid.Matches(Text);
+                if (sepmatches.Count > 0)
+                {
+                    Title = sepmatches[0].ToString();
+                    _titlechanged = true;
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Raised when this workspace should be removed from the UI.
+        /// </summary>
+        public event EventHandler RequestClose;
+        void OnRequestClose()
+        {
+            EventHandler handler = this.RequestClose;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+        private RelayCommand _closeNoteCommand;
+        public RelayCommand CloseNoteCommand { get { return _closeNoteCommand ?? (_closeNoteCommand = new RelayCommand(OnRequestClose)); } }
+
+        #endregion
+
+        #region Minion
+        //Commands from constructor
+        private ObservableCollection<MinionCommandItem> _minionCommands;
+        // local minion instance for this note.
+        private MinionViewModel _noteMinion;
+        public MinionViewModel NoteMinion { get { return _noteMinion ?? (_noteMinion = new MinionViewModel(_minionCommands)); } set { _noteMinion = value; RaisePropertyChanged(); } }
+        //Register and Unregister Minion instances to allow writing to note
         void MinionCollection_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
         {
             if (e.NewItems != null && e.NewItems.Count != 0)
                 foreach (MinionItemViewModel minionItem in e.NewItems)
                 {
                     minionItem.NoteWrite += minionItem_NoteWrite;
-
                 }
 
             if (e.OldItems != null && e.OldItems.Count != 0)
@@ -98,45 +117,22 @@ namespace Scrivener.ViewModel
                     minionItem.NoteWrite -= minionItem_NoteWrite;
                 }
         }
-
+        //fires on event to write note from minion instance with message
         void minionItem_NoteWrite(object sender, MinionArgs e)
         {
             Text += Environment.NewLine + e.Message;
-        }
+        } 
+        #endregion
+      
+        #region QuickItems
+        //Tree
+        private QuickItem _root;
+        public QuickItem Root { get { return _root; } set { _root = value; RaisePropertyChanged(); } }
+        private QuickItem _selectedQuickItem;
+        public QuickItem SelectedQuickItem { get { return _selectedQuickItem; } set { _selectedQuickItem = value; RaisePropertyChanged(); } }
         
-
-        //Test Notify Event. Must be changed to only process a textbox changed event (which needs created).
-        void Note_TextChanged(object sender, EventArgs e)
-        {
-           Regex ip = new Regex(@"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b\s+");
-           Regex sepid = new Regex(@"\b[a-z]{2,3}[0-9]{5,6}\b\s+", RegexOptions.IgnoreCase);
-
-           
-           MatchCollection mc = ip.Matches(Text);
-
-           foreach (Match m in mc)
-           {
-
-           }
-
-           if (_titlechanged == false) //Changes Title to first SEP entered then stops.
-           {
-               MatchCollection sepmatches = sepid.Matches(Text);
-               if (sepmatches.Count > 0)
-               {
-                   Title = sepmatches[0].ToString();
-                   _titlechanged = true;
-               }
-
-           }
-        }
-
-        #region Items
         private RelayCommand _appendQuickItem;
         public RelayCommand AppendQuickItemCommand { get { return _appendQuickItem ?? (_appendQuickItem = new RelayCommand(AppendQuickItem)); } }
-        private RelayCommand _copyQuickItemCommand;
-        public RelayCommand CopyQuickItemCommand { get { return _copyQuickItemCommand ?? (_copyQuickItemCommand = new RelayCommand(CopyQuickItem)); } }
-
         public void AppendQuickItem()
         {
             if (Properties.Settings.Default.DashinNotes == true)
@@ -145,29 +141,29 @@ namespace Scrivener.ViewModel
                     return;
                 else
                     Text += "- " + SelectedQuickItem.Content + System.Environment.NewLine;
-                    //SaveNotes();
+                //SaveNotes();
             }
             else if (Properties.Settings.Default.DashinNotes == false)
             {
                 try
                 {
-                if (SelectedQuickItem.SubItems.Count > 0)
-                    return;
-                else
-                    Text += SelectedQuickItem.Content + System.Environment.NewLine;
-                        //SaveNotes();
+                    if (SelectedQuickItem.SubItems.Count > 0)
+                        return;
+                    else
+                        Text += SelectedQuickItem.Content + System.Environment.NewLine;
+                    //SaveNotes();
                 }
                 catch (Exception e)
                 {
                     //log.Error(e);
                     MetroMessageBox.Show("NOPE!", e.ToString());
 
+                }
             }
         }
-        }
 
-
-
+        private RelayCommand _copyQuickItemCommand;
+        public RelayCommand CopyQuickItemCommand { get { return _copyQuickItemCommand ?? (_copyQuickItemCommand = new RelayCommand(CopyQuickItem)); } }
         public void CopyQuickItem()
         {
             if (_selectedQuickItem != null)
