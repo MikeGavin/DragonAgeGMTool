353cf6184dfda4d5c90b17e4f6e78c99ab2a6afb
J.D. Cain <jd.cain.jr@gmail.com>
2015-02-16 16:28:23 -0500

    Added a treenode of T class for experments

---
+++ NoteTaker/Model/TreeNodeT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrivener.Model
+{
+    /// <summary>
+    /// Source: http://stackoverflow.com/a/11879200
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class TreeNode<T>  
+    {
+        ObservableCollection<TreeNode<T>> Children;
+
+        T Item { get; set; }
+
+        public TreeNode(T item)
+        {
+            Item = item;
+        }
+
+        public TreeNode<T> AddChild(T item)
+        {
+            TreeNode<T> nodeItem = new TreeNode<T>(item);
+            Children.Add(nodeItem);
+            return nodeItem;
+        }
+    }
+}
